c17e79f3a6fa3b3090de75928d6c375bbdc1f8b9
Brian Gantick <bgantick@gmail.com>
2016-06-20 21:15:43 -0400

    Update targeting script

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-window.onload = function(){
+window.onload = function() {
   var anchors = document.getElementsByTagName('a');
   var a = new RegExp('/' + window.location.host + '/');
-  for (var i=0; i<anchors.length; i++){
-    if (!a.test(this.href)) {
+  for (var i = 0; i < anchors.length; i++) {
+    if (!a.test(anchors[i])) {
       anchors[i].setAttribute('target', '_blank');
     }
   }
-}
+};
