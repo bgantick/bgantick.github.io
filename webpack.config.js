ca18c7050afdea3730a8d7aaba8f35dfb47cade5
Brian Gantick <bgantick@gmail.com>
2020-05-10 16:29:13 -0400

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./src/ts/site.ts', './src/scss/site.scss'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.scss'],
+  },
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/site.css'
+    })
+  ]
+};
